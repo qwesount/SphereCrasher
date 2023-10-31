14f302ad0cb0057c285980394f99644eb4c590d3
QWESOUNT <96152899+qwesount@users.noreply.github.com>
2023-10-30 20:32:27 -0600

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.Lemony.gorillatag.Capsule";
+		public const string Name = "Capsule";
+		public const string Version = "1.0.0";
+	}
+}
